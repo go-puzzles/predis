e3b621b9ebf4b9f49a8146d335ad52003703db44
Hoven <hovenyang.best@gmail.com>
2024-08-20 18:16:14 +0800

    update: predis unit test

--- client_test.go
+++ client_test.go
@@ -9,34 +9,77 @@
 package predis
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"testing"
 	"time"
 
 	"github.com/go-puzzles/dialer"
 	"github.com/gomodule/redigo/redis"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestClientCommandDo(t *testing.T) {
 	client := NewRedisClient(dialer.DialRedisPool("localhost:6379", 12, 100))
 
 	res, err := redis.String(client.Do("set", "test-key", 10))
-	if err != nil {
-		t.Errorf("command do err: %v", err)
-		return
-	}
-	fmt.Println("command resp: ", res)
+	assert.NoError(t, err)
+
+	t.Logf("command resp: %v", res)
 
 	res, err = redis.String(client.Do("get", "test-key"))
-	if err != nil {
-		t.Errorf("command do err: %v", err)
-		return
-	}
-	fmt.Println("command resp: ", res)
+	assert.NoError(t, err)
+
+	t.Logf("command resp: %v", res)
 	if res != "10" {
 		t.Error("resp no equal")
 		return
 	}
+}
+
+func TestClientSet(t *testing.T) {
+	client := NewRedisClient(dialer.DialRedisPool("localhost:6379", 13, 100))
+
+	err := client.Set("test-key-set", "test-value")
+	assert.NoError(t, err)
+	var dataStr string
+	err = client.Get("test-key-set", &dataStr)
+	assert.NoError(t, err)
+	assert.Equal(t, "test-value", dataStr)
+	t.Log(dataStr)
+
+	// ================================
+	err = client.Set("test-key-set-byte", []byte("test-value"))
+	assert.NoError(t, err)
+	var dataByte []byte
+	err = client.Get("test-key-set-byte", &dataByte)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("test-value"), dataByte)
+	t.Log(dataByte)
+
+	// ================================
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	err = enc.Encode(map[string]string{"test-key": "test-value"})
+	assert.NoError(t, err)
+
+	b := buf.Bytes()
+	t.Logf("set b: %v", b)
+	err = client.Set("test-gob-set-byte", b)
+	assert.NoError(t, err)
+
+	dataByte = []byte{}
+	err = client.Get("test-gob-set-byte", &dataByte)
+	assert.NoError(t, err)
+
+	t.Log(dataByte)
+
+	dec := gob.NewDecoder(bytes.NewBuffer(dataByte))
+	resp := make(map[string]string)
+	err = dec.Decode(&resp)
+
+	assert.Equal(t, map[string]string{"test-key": "test-value"}, resp)
 }
 
 func TestClientLock(t *testing.T) {
